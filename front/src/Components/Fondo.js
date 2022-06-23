3fe521caa57f595be82360a98003e61a13838bb2
CamiloDuque98 <camilo.dque98@gmail.com>
2022-06-22 22:39:39 -0400

    Se elimino el front

--- front/src/Components/Fondo.js
+++
@@ -1,7 +0,0 @@
-
-import Imagen from "./Img/arbol.PNG"
-function Fondo(){
-    return(<img src={Imagen}/>);
-};
-
-export default Fondo; 
--- front/src/Components/WebImg.js
+++
@@ -1,8 +0,0 @@
-import pic from "./Img/logo.PNG";
-import "./CSS/WebImg.css";
-
-function WebImg(){
-    return <img  className="iMAGEN" src={pic} />
-};
-
-export default WebImg;
