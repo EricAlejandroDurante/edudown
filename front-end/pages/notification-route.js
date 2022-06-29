9c3d6482637c6d2146b411f822f642656577a8e8
CamiloDuque98 <camilo.villar@sansano.usm.cl>
2022-06-29 17:00:41 -0400

    se agrego notificaciones en el front

---
+++ front-end/pages/notification-route.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from "../styles/Home.module.css";
+import Notification from "../component/Notification";
+
+export default function NotificationSide(){
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Edudown</title>
+            </Head>
+            <main className={styles.main}>
+                <h1>Notifique su problema</h1>
+                <Notification/>
+            </main>
+        </div>
+    )
+}
