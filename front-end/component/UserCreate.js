4d137b96126eb3cf616c28c4a3dfcafb4a7a456d
Aguayo25 <javier.aguayo@sansano.usm.cl>
2022-06-29 22:32:29 -0400

    Cambios UserCreate
    cambios n UserCreate

--- front-end/component/UserCreate.js
+++ front-end/component/UserCreate.js
@@ -1,9 +1,12 @@
 import { gql, useMutation } from '@apollo/client'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { Form,Button } from "react-bootstrap"
+
 
 const FormSchema = Yup.object().shape({
   name: Yup.string().required(''),
+  lastName: Yup.string().required(''),
   email: Yup.string().required('').email(),
   RUT: Yup.string().required(''),
   password: Yup.string().required(''),
@@ -12,10 +15,11 @@ const FormSchema = Yup.object().shape({
 })
 
 const CreateUser = gql`
-mutation Mutation($rut: String, $name: String, $email: String, $password: String, $especialidad: String, $edad: Int) {
-  createUser(RUT: $rut, name: $name, email: $email, password: $password, especialidad: $especialidad, edad: $edad) {
+mutation Mutation($rut: String, $name: String, $lastName: String, $email: String, $password: String, $especialidad: String, $edad: Int) {
+  createUser(RUT: $rut, name: $name, lastName: $lastName, email: $email, password: $password, especialidad: $especialidad, edad: $edad) {
     RUT
     name
+    lastName
     email
     password
     especialidad
@@ -32,6 +36,7 @@ export default function Create() {
     initialValues: {
       rut: undefined,
       name: undefined,
+      lastName: undefined,
       email: undefined,
       password: undefined,
       especialidad: undefined,
@@ -50,9 +55,60 @@ export default function Create() {
     //return JSON.stringify(error)
   }
   return (
-    <div>
+    <div className='p-3 mb-2 bg-primary text-white'>
       <h1>Hola</h1>
       <form onSubmit={formik.handleSubmit}>
+
+        <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicRUT" >
+            <Form.Control name='rut' type="text" placeholder="RUT" onChange={formik.handleChange} value={formik.values.rut} required/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Control name='name' type="text" placeholder="Nombre" onChange={formik.handleChange} value={formik.values.name} required/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicLastName">
+            <Form.Control name='lastName' type="text" placeholder="Apellido" onChange={formik.handleChange} value={formik.values.lastName} required/>
+          </Form.Group>
+
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control name='email' type="email" placeholder="Email" onChange={formik.handleChange} value={formik.values.email} required />
+          </Form.Group>
+      
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">    
+            <Form.Control name='password' type="password" placeholder="Contraseña" onChange={formik.handleChange} value={formik.values.password} required/>
+          </Form.Group>
+        
+
+          <Form.Group className="mb-3" controlId="formBasicSpeciality">
+            <Form.Control name='especialidad' type="text" placeholder="Especialidad" onChange={formik.handleChange} value={formik.values.especialidad} required/>
+          </Form.Group>
+  
+
+          <Form.Group className="mb-3" controlId="formBasicAge">
+            <Form.Control name='edad' type="number" placeholder="Edad" onChange={formik.handleChange} value={formik.values.edad} required/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+
+        </div>
+        
+
+
+       
+      </form>
+    </div>
+  )
+
+  /*
         <div >
 
           <input type="text" name='rut' placeholder="RUT" onChange={formik.handleChange} value={formik.values.rut} required />
@@ -63,10 +119,7 @@ export default function Create() {
           <input type="number" name='edad' placeholder="edad" onChange={formik.handleChange} value={formik.values.edad} required />
 
         </div>
-        <div className="flex items-center justify-between mt-4">
+         <div className="flex items-center justify-between mt-4">
           <button className="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none" type='submit'>Crear Usuario</button>
-        </div>
-      </form>
-    </div>
-  )
+        </div> */
 }
